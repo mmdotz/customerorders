fa1aed508203e4c69483b87da45f2a11a1e5af3c
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-17 18:32:03 -0500

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
-  get('/users', { to: 'users#index' } )
-  get('/users', { to: 'users#show' } )
-  get('/users', { to: 'users#index' } )
-  get('/users', { to: 'users#index' } )
+  get     'users/index'   to: 'users#index'
 
+  get     'users/new'     to: 'users#new'
 
-    root 'users#index'
+  get     'users/show'    to: 'users#show'
+
+  post    'users/create'  to: 'users#create'
+
+  put     'users/update'  to: 'users#update'
+
+  delete  'users/destroy' to: 'users#destroy'
+
+  root 'users#index'
 end
